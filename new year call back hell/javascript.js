0e29d96537ba818ae6719715868efaa984e13aba
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2021-10-12 23:35:15 +0530

    callback-hell-independence day

---
+++ new year call back hell/javascript.js
@@ -0,0 +1,46 @@
+let count=11;
+document.querySelector(".msg").innerText=--count;
+
+setTimeout(()=>{
+    document.querySelector(".msg").innerText=--count;
+
+    setTimeout(()=>{
+        document.querySelector(".msg").innerText=--count;
+        
+        setTimeout(()=>{
+            document.querySelector(".msg").innerText=--count;
+            
+            setTimeout(()=>{
+                document.querySelector(".msg").innerText=--count;
+                
+                setTimeout(()=>{
+                    document.querySelector(".msg").innerText=--count;
+                 
+                    setTimeout(()=>{
+                        document.querySelector(".msg").innerText=--count;
+                        
+                        setTimeout(()=>{
+                            document.querySelector(".msg").innerText=--count;
+                           
+                            setTimeout(()=>{
+                                document.querySelector(".msg").innerText=--count;
+                               
+                                setTimeout(()=>{
+                                    document.querySelector(".msg").innerText=--count;
+                                    
+                                   
+                                        
+                                        setTimeout(()=>{
+                                            document.querySelector(".msg").innerText="Happy Independence Day!!!!";
+                                            
+                                        
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
